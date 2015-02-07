f52e5badcc0adb56bb5268a7487a37d521c29788
Thibaut STALIN <thibaut.st@gmail.com>
2015-02-07 19:20:55 +0100

    Part 5 done

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,12 @@
 from django.http.response import HttpResponse
 
 # Create your views here.
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Hello world.")
+    message = {"test_message":"Un message de test. "}
+    return render(request, 'rango/index.html', message)
+
+def about(request):
+    return render(request, 'rango/about.html')
