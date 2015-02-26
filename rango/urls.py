61bdbe98fbea0dd3bf7b5a887245add4ca169ff7
Thibaut STALIN <thibaut.st@gmail.com>
2015-02-26 16:04:51 +0100

    part 12 done

--- rango/urls.py
+++ rango/urls.py
@@ -5,8 +5,5 @@ urlpatterns = patterns('',
                        url(r'^category/(?P<cat_name_slug>[\w\-]+)/$', 'rango.views.category', name='category'),
                        url(r'^add_category/$', 'rango.views.add_category', name='add_category'),
                        url(r'^add_page/(?P<cat_slug>[\w\-]+)/$', 'rango.views.add_page', name='add_page'),
-                       url(r'^register/$', 'rango.views.register', name='register'),
-                       url(r'^login/$', 'rango.views.user_login', name='login'),
-                       url(r'^logout/$', 'rango.views.user_logout', name='logout'),
                        url(r'^about/$', 'rango.views.about', name="about"),
 )
--- rango/views.py
+++ rango/views.py
@@ -88,55 +88,6 @@ def add_page(request, cat_slug):
     return render(request, 'rango/add_page.html', {'form': form, 'category': cat})
 
 
-def register(request):
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
-        else:
-            print user_form.errors, profile_form.errors
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-    return render(request, 'rango/register.html',
-                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/rango/')
-            else:
-                return HttpResponse("Account disabled.")
-        else:
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'rango/login.html')
-
-
-@login_required
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect('/rango/')
-
-
 @login_required
 def about(request):
     return render(request, 'rango/about.html')
